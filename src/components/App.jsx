2859a610b64b6f375ec31cdc7fd93a1935a3a866
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-11-09 18:04:29 +0200

    add ls to loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,9 @@ import { Find } from './Filter';
 import { FormIn } from './Formin.jsx';
 import { ContactList } from './Contact.jsx';
 export function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
   const [filter, setFilter] = useState('');
 
   const onSubmit = evt => {
